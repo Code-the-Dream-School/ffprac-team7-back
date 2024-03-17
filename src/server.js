cc07bdf4781f1d3e0be76fdda2cea8b4d0723a07
Maria S <mariasantiago0109@gmail.com>
2024-03-17 17:53:47 -0400

    Connected to the created team database.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
+require('dotenv').config();
+const connectDB = require('./db/connect')
 const { PORT = 8000 } = process.env;
-const app = require("./app");
+const app = require('./app');
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+
+const start = async () => {
+    try {   
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, listener);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
