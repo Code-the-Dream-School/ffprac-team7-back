dcb3f38116edee8fcf6e8e6196bfdb27f0f88fba
Maria S <mariasantiago0109@gmail.com>
2024-03-23 17:45:50 -0400

    Removed all the console log statements present in the item controller.

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -9,7 +9,6 @@ const createItem = async (req, res) => {
         res.status(StatusCodes.CREATED).json({msg:'The item has been created.', item});
 
     } catch (error) {
-        console.log(error.message);
         res.status(StatusCodes.INTERNAL_SERVER_ERROR);
     }
 }
@@ -24,7 +23,6 @@ const getItem = async (req, res) => {
         const item = await Item.findOne({_id: itemId});
 
         if (!item) {
-            console.log('Item not found.');
             res.status(StatusCodes.NOT_FOUND).json({msg:`The item with id:${itemId} was not found.`})
 
         } else {
@@ -32,7 +30,6 @@ const getItem = async (req, res) => {
         }
 
     } catch (error) {
-        console.log(error.message);
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error.message);
     }
 }
@@ -44,7 +41,6 @@ const getAllItems = async (req, res) => {
         res.status(StatusCodes.OK).json({items, count:items.length});
      
     } catch (error) {
-        console.log(error.message);
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error.message); 
     }
 }
@@ -60,11 +56,9 @@ const getAllItemsByUser = async (req, res) => {
         const user = await User.findOne({_id: userId});
 
         if (!user) {
-            console.log(`User not found.`);
             res.status(StatusCodes.NOT_FOUND).json({msg:`The user with id:${userId} was not found.`});
 
         } else if (items.length === 0) {
-            console.log(`This user has not reported any items yet.`);
             res.status(StatusCodes.OK).json({msg:'This user has not reported any items yet.'});
 
         } else {
@@ -72,7 +66,6 @@ const getAllItemsByUser = async (req, res) => {
         }
 
     } catch (error) {
-        console.log(error.message);
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error.message);   
     }
 }
@@ -88,40 +81,32 @@ const updateItem = async (req, res) => {
         const item = await Item.findOneAndUpdate({_id: itemId}, req.body, {new:true, runValidators:true});
 
         if (!item) {
-            console.log('Item not found.');
             res.status(StatusCodes.NOT_FOUND).json({msg:`The item with id:${itemId} was not found.`})
 
         } else if (title === '')  {
-            console.log(`Please provide item title.`);
             res.status(StatusCodes.BAD_REQUEST).json({msg:`Please provide item title.`});
 
         } else if (description === '') {
-            console.log(`Please provide item description.`);
             res.status(StatusCodes.BAD_REQUEST).json({msg:`Please provide item description.`});
 
         } else if (location === '') {
-            console.log(`Please provide the location where the item was lost.`);
             res.status(StatusCodes.BAD_REQUEST).json({msg:`Please provide the location where the item was lost.`});
 
         } else if (lost === '') {
-            console.log(`Please indicate if the item is lost or not.`);
             res.status(StatusCodes.BAD_REQUEST).json({msg:`Please indicate if the item is lost or not.`});
 
             // The code below will be updated. The current conditional is not working when (lost===false).
         } else if (lost === false) {
             if (dateClaimed === '') {
-                console.log(`Please provide the date the item is being claimed.`);
                 res.status(StatusCodes.BAD_REQUEST).json({msg:`Please provide the date the item is being claimed.`});
 
             } else if (claimedBy === '') {
-                console.log(`Please provide the id of the person claiming the item.`);
                 res.status(StatusCodes.BAD_REQUEST).json({msg:`Please provide the id of the person claiming the item.`});
             }
         }
         res.status(StatusCodes.OK).json({msg:'The item has been updated.', item});  
 
     } catch (error) {
-        console.log(error.message);
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error.message); 
     }
 }
@@ -136,7 +121,6 @@ const deleteItem = async (req, res) => {
         const item = await Item.findByIdAndDelete(itemId);
 
         if (!item) {
-            console.log('Item not found.');
             res.status(StatusCodes.NOT_FOUND).json({msg:`The item with id:${itemId} was not found.`});
 
         } else {
@@ -144,7 +128,6 @@ const deleteItem = async (req, res) => {
         }
 
    } catch (error) {
-        console.log(error.message);
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error.message); 
    }
 }
