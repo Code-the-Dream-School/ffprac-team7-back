a9bb8a974ae6d54871980d62ab0fb05b0f7cdde0
Maria S <mariasantiago0109@gmail.com>
2024-04-14 14:45:13 -0400

    Installed swagger-jsdoc and swagger-ui-express, created API specification and the swagger UI docs page, and defined the root information for the StuffFindr API in order to write the documentation.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,38 @@
 const express = require("express");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const app = express();
 const cors = require("cors");
 const favicon = require("express-favicon");
 const logger = require("morgan");
 
+
+// swagger documentation
+const swaggerDefinition = {
+    openapi: "3.0.0",
+    info: {
+        title: "Express API for StuffFindr",
+        version: "1.0.0",
+        description: "This is a REST API app created with Express."
+    },
+    servers: [
+        {   url: "http://localhost:8000",
+            description: "Development server",
+        }
+    ]
+};
+
+const options = {
+    swaggerDefinition,
+    apis: ["./routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+// swagger Ui page
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// routers
 const mainRouter = require("./routes/mainRouter.js");
 const userRouter = require("./routes/userRouter.js");
 const itemsRouter = require("./routes/itemRouter.js");
