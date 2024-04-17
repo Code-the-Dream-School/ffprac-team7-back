bff08333eb454641405d6a740a1a767966a995aa
devUser110 <jr.devon@gmail.com>
2024-04-17 19:51:46 -0400

    update authentication middleware and userController

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,27 +1,26 @@
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-const { StatusCodes } = require('http-status-codes');
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../errors");
 
 const auth = async (req, res, next) => {
-    const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        res.status(StatusCodes.UNAUTHORIZED).json({msg: 'Authentication Invalid'});
+  try {
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      throw new UnauthenticatedError("Authentication Invalid");
     }
 
-    const token = authHeader.split(' ')[1];
+    const token = authHeader.split(" ")[1];
 
-    try {
-        const payload = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = {
-            userId: payload.userId,
-            username: payload.username
-        }
-        next();
-
-    } catch (error) {
-        res.status(StatusCodes.UNAUTHORIZED).json({msg: 'Authentication Invalid'});
-    }
-}
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = {
+      userId: payload.userId,
+      username: payload.username,
+    };
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = auth;
