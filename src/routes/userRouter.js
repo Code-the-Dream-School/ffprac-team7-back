04dba9d5235cb6f1a1137e44371ed8f0a96fc4a9
Maria S <mariasantiago0109@gmail.com>
2024-04-20 15:31:08 -0400

    Created swagger documentation for the getUserByUsername GET operation and the uploadProfilePicture POST operation. Added an Authorization component to the swagger documentation to be used with the uploadProfilePicture operation route. Updated some of the documentation responses for some of the routes with the corresponding operation data that was missing.

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -12,6 +12,27 @@ const {
   uploadProfilePicture,
 } = require("../controllers/userController");
 
+// Swagger Authorization components
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The user ID.
+ *           example: 0
+ *         name:
+ *           type: string
+ *           description: The user's name.
+ *           example: Leanne Graham
+ */
+
+// User routes with Swagger documentation
 /**
  * @swagger
  * /users/signup:
@@ -43,6 +64,14 @@ const {
  *                 type: string
  *                 description: User's location
  *                 example: Los Angeles, CA
+ *               phoneNumber:
+ *                 type: string
+ *                 description: User's phone number.
+ *                 example: (980) 111 - 1234
+ *               profilePicture:
+ *                 type: string
+ *                 description: Uploaded profile picture
+ *                 example: profile-pic.img
  *     responses:
  *       '200':
  *         description: Response after successfully creating an account.
@@ -60,6 +89,10 @@ const {
  *                         type: string
  *                         description: User's username.
  *                         example: lilytest
+ *                       userId:
+ *                         type: string
+ *                         description: account holder's userId.
+ *                         example: 661e5f1684beefd1560e2e29
  *                       email:
  *                         type: string
  *                         description: User's email address.
@@ -68,6 +101,14 @@ const {
  *                         type: string
  *                         description: User's inputted location.
  *                         example: Los Angeles, CA
+ *                       phoneNumber:
+ *                         type: string
+ *                         description: User's phone number.
+ *                         example: (980) 111 - 1234
+ *                       profilePicture:
+ *                         type: string
+ *                         description: Uploaded profile picture
+ *                         example: profile-pic.img
  *                       token:
  *                         type: string
  *                         description: User token created after signing up.
@@ -115,6 +156,10 @@ router.post("/signup", signup);
  *                         type: string
  *                         description: User's username.
  *                         example: lilytest
+ *                       userId:
+ *                         type: string
+ *                         description: account holder's userId.
+ *                         example: 661e5f1684beefd1560e2e29
  *                       email:
  *                         type: string
  *                         description: User's email address.
@@ -123,18 +168,159 @@ router.post("/signup", signup);
  *                         type: string
  *                         description: User's inputted location.
  *                         example: Los Angeles, CA
+ *                       phoneNumber:
+ *                         type: string
+ *                         description: User's phone number.
+ *                         example: (980) 111 - 1234
+ *                       profilePicture:
+ *                         type: string
+ *                         description: Uploaded profile picture
+ *                         example: profile-pic.img
  *                       token:
  *                         type: string
  *                         description: User token created after loggin in, which will be needed for lost items management
  *                         example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjFlNWYxNjg0ZGRlZmQxNTYwZTJlMjkiLCJ1c2VybmFtZSI6Im5pbmF0ZXN0IiwiaWF0IjoxNzEzMjY2NDU1LCJleHAiOjE3MTU4NTp0NTV9.Z_XLzqwiWAFTL6It16mwDEeq1zH9NcLh8H8P15vmpL8"
 */
 router.post("/login", login);
+
+/**
+ * @swagger
+ * /users/upload-profile-picture:
+ *   post:
+ *     tags: 
+ *        - User Routes
+ *     summary: Upload profile picture to StuffFindr account
+ *     description: Upload profile picture to StuffFindr account
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: header
+ *         name: token
+ *         required: true
+ *         description: The token that is created when StuffFindr user signs in, which contains the payload with the userId and username
+ *         schema:
+ *           type: object
+ *           properties:
+ *             user:
+ *               type: object
+ *               properties:
+ *                 userId:
+ *                   type: string
+ *                 username:
+ *                   type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               profilePicture:
+ *                 type: string
+ *                 format: binary
+ *                 description: User's profile picture file.
+ *                 example: profile-pic.img
+ *               userId:
+ *                 type: string
+ *                 description: This userId value is obtained from the token payload as req.user.userId
+ *     responses:
+ *       '200':
+ *         description: Response after successfully uploading a profil picture. 
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       message:
+ *                         type: string
+ *                         example: "Profile picture uploaded successfully"
+ *                       username:
+ *                         type: string
+ *                         description: User's username.
+ *                         example: lilytest
+ *                       email:
+ *                         type: string
+ *                         description: User's email address.
+ *                         example: lilytest@test.com
+ *                       location:
+ *                         type: string
+ *                         description: User's inputted location.
+ *                         example: Los Angeles, CA
+ *                       phoneNumber:
+ *                         type: string
+ *                         description: User's phone number.
+ *                         example: (980) 111 - 1234
+ *                       profilePicture:
+ *                         type: string
+ *                         description: Uploaded profile picture
+ *                         example: profile-pic.img
+*/
 router.post(
   "/upload-profile-picture",
   authenticateUser,
   upload.single("profilePicture"),
   uploadProfilePicture
 );
+
+/**
+ * @swagger
+ * /users/:username:
+ *   get:
+ *     tags: 
+ *        - User Routes
+ *     summary: Find a StuffFindr account by username
+ *     description: Find a user's StuffFindr account by using their username. 
+ *     parameters:
+ *       - in: path
+ *         name: username
+ *         required: true
+ *         description: StuffFindr username of the user to retrieve account. 
+ *         schema:
+ *           type: string
+ *         example: lilytest
+ *     responses:
+ *       '200':
+ *         description: Response after successfully finding a StuffFindr user. 
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       username:
+ *                         type: string
+ *                         description: User's username.
+ *                         example: lilytest
+ *                       userId:
+ *                         type: string
+ *                         description: account holder's userId.
+ *                         example: 661e5f1684beefd1560e2e29
+ *                       email:
+ *                         type: string
+ *                         description: User's email address.
+ *                         example: lilytest@test.com
+ *                       location:
+ *                         type: string
+ *                         description: User's inputted location.
+ *                         example: Los Angeles, CA
+ *                       phoneNumber:
+ *                         type: string
+ *                         description: User's phone number.
+ *                         example: (980) 111 - 1234
+ *                       profilePicture:
+ *                         type: string
+ *                         description: Uploaded profile picture
+ *                         example: profile-pic.img 
+*/
 router.get("/:username", authenticateUser, getUserByUsername);
 
 /**
@@ -197,6 +383,14 @@ router.get("/:username", authenticateUser, getUserByUsername);
  *                         type: string
  *                         description: User's inputted location.
  *                         example: Miami, FL
+ *                       phoneNumber:
+ *                         type: string
+ *                         description: User's phone number.
+ *                         example: (980) 111 - 1234
+ *                       profilePicture:
+ *                         type: string
+ *                         description: Uploaded profile picture
+ *                         example: profile-pic.img
 */
 router.put("/:userId", authenticateUser, updateUser);
 
@@ -244,6 +438,14 @@ router.put("/:userId", authenticateUser, updateUser);
  *                         type: string
  *                         description: User's inputted location.
  *                         example: Los Angeles, CA
+ *                       phoneNumber:
+ *                         type: string
+ *                         description: User's phone number.
+ *                         example: (980) 111 - 1234
+ *                       profilePicture:
+ *                         type: string
+ *                         description: Uploaded profile picture
+ *                         example: profile-pic.img
 */
 router.delete("/:userId", authenticateUser, deleteUser);
 
